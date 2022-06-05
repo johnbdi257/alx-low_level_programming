f185ce273489e43fae46e722f8d50f1707d4a3b4
johnbdi257 <kevinbdi384@gmai.com>
2022-06-05 20:27:38 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * HAHAHAHA  NDABAFUSE.
- * NTAVYO MUZI MWESE HAHAHAHA.
- *MURAKOZE KUNTESHA UMWANYA.
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
