b34001ca7a55a6e211c4337cd1c6ebc99437943e
johnbdi257 <kevinbdi384@gmai.com>
2022-06-05 20:16:40 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-/**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
- */
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*Alloo murakomeye hahahahaha ndabafuse mwese ntavyo muzi hahahahaha*/
+ int main()
+ {
+     printf("Programming is like building a multilingual puzzle\n");
+     return 0;
+ }
+
