b5af8cba757edf55a5cec40ab75ec23d60e901e4
johnbdi257 <kevinbdi384@gmai.com>
2022-06-05 20:43:38 +0300

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Hahahahahahah
+ *
+ * Return: nukuri mumbabarire muga vyandambiye kwl 0 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+return (1);
+}
