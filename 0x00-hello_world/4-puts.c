b9201296238941ddbfd8d1b9871f2fe0595d9342
johnbdi257 <kevinbdi384@gmai.com>
2022-06-05 20:24:11 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-
-/*Alloo murakomeye hahahahaha ndabafuse mwese ntavyo muzi hahahahaha*/
- int main()
- {
-     puts("\"Programming is like building a multilingual puzzle");
-     return 0;
- }
-
+/**
+ * HAHAHAHA  NDABAFUSE
+ * NTAVYO MUZI MWESE HAHAHAHA.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
