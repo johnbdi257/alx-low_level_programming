fedb65b8637db4b22868847729825f8c98019371
johnbdi257 <kevinbdi384@gmai.com>
2022-06-05 20:11:06 +0300

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*Alloo murakomeye hahahahaha ndabafuse mwese ntavyo muzi hahahahaha*/
+ int main()
+ {
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return 0;
+ }
