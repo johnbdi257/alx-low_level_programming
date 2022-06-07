4d9c0778aadd946b9a713fe70ed303c634a093b2
johnbdi257 <kevinbdi384@gmai.com>
2022-06-08 00:03:38 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * main - Entry point
@@ -7,15 +7,10 @@
   */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('M');
+	_putchar('A');
+	_putchar('I');
+	_putchar('N');
 	_putchar('\n');
 
 	return (0);
